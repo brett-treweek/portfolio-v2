306d7cc9de9dd98fc53551247a0f56b6d34d10a6
Brett Treweek <bretttrew@gmail.com>
2022-04-07 13:00:43 +0800

    fresh start after new figma design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import Header from './components/Header';
-import GlobalStyles from './components/styles/Global';
-// import { useRef } from 'react';
-
 function App() {
 
-	const navClickHandler = (navclick) => {
-		
-		console.log("Nav clicked!!!!!", navclick);
-	}
 	
 
 	return (
 		<>
-			<GlobalStyles />
-			<Header onNavigate={navClickHandler}/>
-			<div
-			style={ {height: "10000px", display: 'flex', flexDirection: 'column', justifyContent: 'space-between'} }
-			>
-				<p>This is This</p>
-				<p>Hello World</p>
-			</div>
+			
 		</>
 	);
 }
